ff6abe570b647ed97de7e0a7b5dfeeaa2d21df35
ruthreyal <r.reyes.alvarez@gmail.com>
2025-06-08 18:32:26 +0200

    ajuste cargar usuarios

--- src/pages/admin/AdminUsuarios.jsx
+++ src/pages/admin/AdminUsuarios.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import usuarioService from "../../services/usuarioService";
 import "../../styles/adminUsuarios.css";
 import FormularioCrearUsuario from "./FormularioCrearUsuario";
@@ -14,15 +14,19 @@ const AdminUsuarios = () => {
   const token = localStorage.getItem("token");
 
   const cargarUsuarios = useCallback(async () => {
-  try {
-    const data = await usuarioService.getTodosUsuarios(token);
-    setUsuarios(data);
-  } catch (error) {
-    setMensaje("Error al cargar usuarios");
-  } finally {
-    setLoading(false);
-  }
-}, [token]);
+    try {
+      const data = await usuarioService.getTodosUsuarios(token);
+      setUsuarios(data);
+    } catch (error) {
+      setMensaje("Error al cargar usuarios");
+    } finally {
+      setLoading(false);
+    }
+  }, [token]);
+
+  useEffect(() => {
+    cargarUsuarios();
+  }, [cargarUsuarios]);
 
   const usuariosFiltrados = usuarios.filter((usuario) =>
     usuario.nombre.toLowerCase().includes(busqueda.toLowerCase()) ||
